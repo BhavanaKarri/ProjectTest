44306022afffaa24072927c52341d3f42feca317
BhavanaKarri <BhavanaKarri@my.unt.edu>
2017-11-16 11:14:04 -0600

    Update Palindromes.java

--- Palindromes.java
+++ Palindromes.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 import java.io.File;
 
 class Palindromes {
-     public static void main(String[] args) throws Exception 
+
+    public static void main(String[] args) throws Exception 
 {
         String pathname = "C:/Users/Bhavana's/Desktop/SE Exam/program";
         File dictionary = new File(pathname);
@@ -14,5 +15,18 @@ class Palindromes {
 {
         throw new IllegalStateException("I wouldn't expect a word to be zero-length");
     }
+   
 
-    
+ if(theString.length() == 1) 
+{
+        System.out.println(word);
+    } else 
+{
+        char[] wordArr = theString.replaceAll("\\s", "").toLowerCase().toCharArray();
+        for(int i = 0, j = wordArr.length - 1; i < wordArr.length / 2; i++, j--) {
+            if(wordArr[i] = wordArr[j]) {
+               System.out.println(word);
+}
+}
+}
+}
