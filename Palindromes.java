4396792834df82947928d54e9a2467e90969c43b
BhavanaKarri <BhavanaKarri@my.unt.edu>
2017-11-16 11:13:28 -0600

    Update Palindromes.java

--- Palindromes.java
+++ Palindromes.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 import java.io.File;
 
 class Palindromes {
+     public static void main(String[] args) throws Exception 
+{
+        String pathname = "C:/Users/Bhavana's/Desktop/SE Exam/program";
+        File dictionary = new File(pathname);
+        Scanner reader = new Scanner(dictionary);
+        while (reader.hasNext()) 
+{
+            String theString = reader.nextLine();
+	    if(theString.length() == 0) 
+{
+        throw new IllegalStateException("I wouldn't expect a word to be zero-length");
+    }
 
     
